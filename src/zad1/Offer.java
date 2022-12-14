c89df866e6597b7c1ef4ea76bd0947f002cd7a6e
CornifersWife <irys.latosz@gmail.com>
2022-12-14 09:23:31 +0100

    constructor offer

--- src/zad1/Offer.java
+++ src/zad1/Offer.java
@@ -3,31 +3,24 @@ package zad1;
 import java.util.*;
 
 public class Offer {
-    Locale locale;
-    
+    Locale lokalizacja;
+    String kraj; // w jezyku kontrahenta
+    Date data_wyjazdu;  //daty w RRRR-MM-DD
+    Date data_powrotu;
+    String miejsce; // w jezyku kontrahenta
+    double cena;
+    String symbol_waluty;
     public Offer(String input) {
 
         String[] data = input.split("\t");
         String[] ToLocale = data[0].split("_");
-        locale = new Locale(ToLocale[0], ToLocale[1]);
-
-    }
-
-    public Locale getLocale() {
-        return
-                new Locale() data[0];
-    }
-
-    public String getCountry() {
-        return data[1];
-    }
-
-    public String getDateStart() {
-        return data[2];
-    }
-
-    public String getDateEnd() {
-        return data[3];
+        lokalizacja = new Locale(ToLocale[0], ToLocale[1]);
+        kraj = data[1];
+        data_wyjazdu = new Date(data[2]);
+        data_powrotu = new Date(data[3]);
+        miejsce = data[4];
+        cena = Double.parseDouble(data[5]);
+        symbol_waluty = data[6];
     }
 
     public String getPlace() {
