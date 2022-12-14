6141a5747b7e6bd69e77407adfde7b6d7b4203b5
CornifersWife <irys.latosz@gmail.com>
2022-12-14 09:13:43 +0100

    Update Offer.java
    locale_1

--- src/zad1/Offer.java
+++ src/zad1/Offer.java
@@ -1,29 +1,43 @@
 package zad1;
 
+import java.util.*;
+
 public class Offer {
-    String[] data;
-    public Offer(String input){
-        data = input.split("\t");
+    Locale locale;
+    
+    public Offer(String input) {
+
+        String[] data = input.split("\t");
+        String[] ToLocale = data[0].split("_");
+        locale = new Locale(ToLocale[0], ToLocale[1]);
+
     }
 
-    public String getLocale() {
-        return data[0];
+    public Locale getLocale() {
+        return
+                new Locale() data[0];
     }
+
     public String getCountry() {
         return data[1];
     }
+
     public String getDateStart() {
         return data[2];
     }
+
     public String getDateEnd() {
         return data[3];
     }
+
     public String getPlace() {
         return data[4];
     }
+
     public double getPrice() {
         return Double.parseDouble(data[5]);
     }
+
     public String getCurrency() {
         return data[6];
     }
